4f81d39f569136d4776c8b8f6510ace3a669e7c6
hijera <hi-jera@yandex.ru>
2020-05-29 13:06:52 +0300

    Форматирование + фиксы

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
 import Stars from './components/Stars.jsx'
+
 function App() {
-  return (
-  <>
-    <Stars count={5} />
-    <Stars count={4} />
-    <Stars count={3} />
-    <Stars count={2} />
-    <Stars count={1} />
-    <Stars count={0}/>
-    <Stars />
-    <Stars count={"abc"} />
-  </>
-  );
+    return (
+        <>
+            <Stars count={5}/>
+            <Stars count={4}/>
+            <Stars count={3}/>
+            <Stars count={2}/>
+            <Stars count={1}/>
+            <Stars count={0}/>
+            <Stars/>
+            <Stars count={"abc"}/>
+        </>
+    );
 }
 
 export default App;
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Star from './Star'
 import shortid from 'shortid'
+
 Stars.propTypes = {
     count: PropTypes.number
 };
 
 function Stars(props) {
-    const { count } =props;
+    const {count} = props;
     return (
         <>
-        { (typeof count === 'number') && (count<=5) && (count>=1) &&
-        <ul className="card-body-stars u-clearfix">
-           { [...Array(count)].map(item => <Star key={shortid.generate()}/>) }
-        </ul>
-        }
+            {(typeof count === 'number') && (count <= 5) && (count >= 1) &&
+            <ul className="card-body-stars u-clearfix">
+                {[...Array(count)].map(item => <Star key={shortid.generate()}/>)}
+            </ul>
+            }
         </>
     );
 }
 
-Stars.defaultProps={
+Stars.defaultProps = {
     count: 0
 };
 
